0fafaf680d70d809001ebf2fe3866ef589aa20e7
nkoleff <nk.kolev6@gmail.com>
2018-07-19 19:01:32 +0300

    Adding functions and read for service key in terminal

--- checkforzombies.sh
+++ checkforzombies.sh
@@ -1,43 +1,25 @@
 #!/bin/bash
 
 #check for zobie processes
+echo "Enter your Pagerduty service key:"
+read -s service_key
+
 zombies=`ps -aux | awk '{ if ($8 ~ /Z/) { print } }'`
 
-#add current date to variable
 date=$(date +"%Y-%m-%d %H:%M")
 
-if [ -z "$zombies" ]; then 
-	echo "there are no zombie processes"
-	#if there are no zombie processes resolve current in state
+#functions for Pagerduty alerts resolve and trigger
 
-	while read k; do
-
-		curl -H "Content-type: application/json" -X POST \
-	    -d '{    
-			"service_key": "YOUR-KEY-HERE",
-			"incident_key": "Zombie-'$k'",
-			"event_type": "resolve",
-			"description": "Nikolay fixed the Zombie-'$k'.",
-			"details": {
-			"fixed at": "'"$date"'"
-			}
-	    }' \
-	    "https://events.pagerduty.com/generic/2010-04-15/create_event.json"
-
-	done < <(comm -23 <( awk '{print $2}' /home/kolev/Documents/zombie_state.txt | sort)  <(ps -aux | awk '{ if ($8 ~ /Z/) { print $2} }' | sort))
-
-else
+trigger () {
 
 	if [ ! -f /home/kolev/Documents/zombie_state.txt ]; then
-		echo "file is not found"
+		#create alerts when file is not found as there are some zombie processes
 
-    	#create alerts when file is not found as there are some zombie processes
-
-    	while read p; do
-  			
-    		curl -H "Content-Type: application/json" -X POST \
+		while read p; do
+	  			
+			curl -H "Content-Type: application/json" -X POST \
 	    	-d '{
-	    		"service_key": "YOUR-KEY-HERE", 
+	    		"service_key": "'$service_key'", 
 	    		"incident_key": "Zombie-'$p'",
 	    		"event_type": "trigger", 
 	    		"description": "Zombie processes found Zombie-'$p'.", 
@@ -50,34 +32,15 @@ else
 	    	https://events.pagerduty.com/generic/2010-04-15/create_event.json
 
 		done < <(ps -aux | awk '{ if ($8 ~ /Z/) { print $2} }')
-    	
-    else
-		echo "file is found"    	
-    	#when file exist first resolve old zombie alerts
 
-    	while read d; do
+	else 
+		#create new alerts for the new zombies if file exists
 
-    		curl -v -H "Content-type: application/json" -X POST \
-		    -d '{    
-				"service_key": "YOUR-KEY-HERE",
-				"incident_key": "Zombie-'$d'",
-				"event_type": "resolve",
-				"description": "Nikolay fixed the Zombie-'$d'.",
-				"details": {
-				"fixed at": "'"$date"'"
-				}
-		    }' \
-		    "https://events.pagerduty.com/generic/2010-04-15/create_event.json"
-
-    	done < <(comm -23 <( awk '{print $2}' /home/kolev/Documents/zombie_state.txt | sort)  <(ps -aux | awk '{ if ($8 ~ /Z/) { print $2} }' | sort))
-
-    	#now create new alerts for the new zombies
-
-    	while read w; do
+		while read w; do
 
     		curl -H "Content-Type: application/json" -X POST \
 	    	-d '{
-	    		"service_key": "YOUR-KEY-HERE", 
+	    		"service_key": "'$service_key'", 
 	    		"incident_key": "Zombie-'$w'",
 	    		"event_type": "trigger", 
 	    		"description": "Zombie processes found Zombie-'$w'.", 
@@ -91,9 +54,56 @@ else
 
     	done < <(comm -13 <( awk '{print $2}' /home/kolev/Documents/zombie_state.txt | sort)  <(ps -aux | awk '{ if ($8 ~ /Z/) { print $2} }' | sort))
 
+    fi
+
+}
+
+resolve () {
+
+	while read k; do
+
+		curl -H "Content-type: application/json" -X POST \
+	    -d '{    
+			"service_key": "'$service_key'",
+			"incident_key": "Zombie-'$k'",
+			"event_type": "resolve",
+			"description": "Nikolay fixed the Zombie-'$k'.",
+			"details": {
+			"fixed at": "'"$date"'"
+			}
+	    }' \
+	    "https://events.pagerduty.com/generic/2010-04-15/create_event.json"
+
+	done < <(comm -23 <( awk '{print $2}' /home/kolev/Documents/zombie_state.txt | sort)  <(ps -aux | awk '{ if ($8 ~ /Z/) { print $2} }' | sort))
+
+}
+
+#run 
+
+if [ -z "$zombies" ]; then 
+	#if there are no zombie processes resolve current in state
+
+	resolve
+
+else
+
+	if [ ! -f /home/kolev/Documents/zombie_state.txt ]; then
+
+    	#create alerts when file is not found as there are some zombie processes
+
+    	trigger
+    	
+    else   	
+    	#when file exist first resolve old zombie alerts
+
+    	resolve
+
+    	#now create new alerts for the new zombies
+
+    	trigger
+
 	fi
 
 fi
 
-#create state file
-zombies_state=`ps -aux | awk '{ if ($8 ~ /Z/) { print } }' > /home/kolev/Documents/zombie_state.txt`
+#zombies_state=`ps -aux | awk '{ if ($8 ~ /Z/) { print } }' > /home/kolev/Documents/zombie_state.txt`
