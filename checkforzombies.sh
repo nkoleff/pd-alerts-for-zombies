33de5848e5e2d63a9b048af59187058c065e7339
nkoleff <nk.kolev6@gmail.com>
2018-07-19 19:29:43 +0300

    Create the state file in start of script

--- checkforzombies.sh
+++ checkforzombies.sh
@@ -4,61 +4,46 @@
 echo "Enter your Pagerduty service key:"
 read -s service_key
 
+#create the state file
+touch /home/kolev/Documents/zombie_state.txt
+
+#check for zombie processes and add them to variable
 zombies=`ps -aux | awk '{ if ($8 ~ /Z/) { print } }'`
 
+#add current date and time to variable
 date=$(date +"%Y-%m-%d %H:%M")
+
 
 #functions for Pagerduty alerts resolve and trigger
 
 trigger () {
 
-	if [ ! -f /home/kolev/Documents/zombie_state.txt ]; then
-		#create alerts when file is not found as there are some zombie processes
+	#create alerts for zombies
 
-		while read p; do
-	  			
-			curl -H "Content-Type: application/json" -X POST \
-	    	-d '{
-	    		"service_key": "'$service_key'", 
-	    		"incident_key": "Zombie-'$p'",
-	    		"event_type": "trigger", 
-	    		"description": "Zombie processes found Zombie-'$p'.", 
-	    		"client": "Sample Monitoring Service",
-	    		"client_url": "https://monitoring.service.com",
-	    		"details": { 
-	    			"device": "Nikolay laptop" 
-	    		}
-	    	}' \
-	    	https://events.pagerduty.com/generic/2010-04-15/create_event.json
+	while read p; do
+  			
+		curl -H "Content-Type: application/json" -X POST \
+    	-d '{
+    		"service_key": "'$service_key'", 
+    		"incident_key": "Zombie-'$p'",
+    		"event_type": "trigger", 
+    		"description": "Zombie processes found Zombie-'$p'.", 
+    		"client": "Sample Monitoring Service",
+    		"client_url": "https://monitoring.service.com",
+    		"details": { 
+    			"device": "Nikolay laptop" 
+    		}
+    	}' \
+    	https://events.pagerduty.com/generic/2010-04-15/create_event.json
 
-		done < <(ps -aux | awk '{ if ($8 ~ /Z/) { print $2} }')
+	done < <(comm -13 <( awk '{print $2}' /home/kolev/Documents/zombie_state.txt | sort)  <(ps -aux | awk '{ if ($8 ~ /Z/) { print $2} }' | sort))
 
-	else 
-		#create new alerts for the new zombies if file exists
-
-		while read w; do
-
-    		curl -H "Content-Type: application/json" -X POST \
-	    	-d '{
-	    		"service_key": "'$service_key'", 
-	    		"incident_key": "Zombie-'$w'",
-	    		"event_type": "trigger", 
-	    		"description": "Zombie processes found Zombie-'$w'.", 
-	    		"client": "Sample Monitoring Service",
-	    		"client_url": "https://monitoring.service.com",
-	    		"details": { 
-	    			"device": "Nikolay laptop" 
-	    		}
-	    	}' \
-	    	https://events.pagerduty.com/generic/2010-04-15/create_event.json
-
-    	done < <(comm -13 <( awk '{print $2}' /home/kolev/Documents/zombie_state.txt | sort)  <(ps -aux | awk '{ if ($8 ~ /Z/) { print $2} }' | sort))
-
-    fi
 
 }
 
 resolve () {
+
+	#resolve alerts for zombies
 
 	while read k; do
 
@@ -78,7 +63,7 @@ resolve () {
 
 }
 
-#run 
+#run
 
 if [ -z "$zombies" ]; then 
 	#if there are no zombie processes resolve current in state
@@ -106,4 +91,4 @@ else
 
 fi
 
-#zombies_state=`ps -aux | awk '{ if ($8 ~ /Z/) { print } }' > /home/kolev/Documents/zombie_state.txt`
+zombies_state=`ps -aux | awk '{ if ($8 ~ /Z/) { print } }' > /home/kolev/Documents/zombie_state.txt`
