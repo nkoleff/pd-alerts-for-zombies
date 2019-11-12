67d151faf8da05880358c5165498097627b756d1
Nikolay Kolev <nk.kolev6@gmail.com>
2019-11-12 15:23:12 +0200

    Changes

--- checkforzombies.sh
+++ checkforzombies.sh
@@ -5,7 +5,7 @@ echo "Enter your Pagerduty service key:"
 read -s service_key
 
 #create the state file if it does not exist
-path="/home/kolev/Documents/zombie_state.txt"
+path="/your-path-to/zombie_state.txt"
 if [ ! -f $path ]; then
 	touch $path
 fi
@@ -16,15 +16,11 @@ zombies=`ps -aux | awk '{ if ($8 ~ /Z/) { print } }'`
 #add current date and time to variable
 date=$(date +"%Y-%m-%d %H:%M")
 
-
 #functions for Pagerduty alerts resolve and trigger
 
 trigger () {
-
 	#create alerts for zombies
-
 	while read p; do
-  			
 		curl -H "Content-Type: application/json" -X POST \
     	-d '{
     		"service_key": "'$service_key'", 
@@ -34,53 +30,37 @@ trigger () {
     		"client": "Sample Monitoring Service",
     		"client_url": "https://monitoring.service.com",
     		"details": { 
-    			"device": "Nikolay laptop" 
+    			"device": "your-device" 
     		}
     	}' \
     	https://events.pagerduty.com/generic/2010-04-15/create_event.json
-
 	done < <(comm -13 <( awk '{print $2}' $path | sort)  <(ps -aux | awk '{ if ($8 ~ /Z/) { print $2} }' | sort))
-
-
 }
 
 resolve () {
-
 	#resolve alerts for zombies
-
 	while read k; do
-
 		curl -H "Content-type: application/json" -X POST \
 	    -d '{    
 			"service_key": "'$service_key'",
 			"incident_key": "Zombie-'$k'",
 			"event_type": "resolve",
-			"description": "Nikolay fixed the Zombie-'$k'.",
+			"description": "Your description for fixing the Zombie-'$k'.",
 			"details": {
 			"fixed at": "'"$date"'"
 			}
 	    }' \
 	    "https://events.pagerduty.com/generic/2010-04-15/create_event.json"
-
 	done < <(comm -23 <( awk '{print $2}' $path | sort)  <(ps -aux | awk '{ if ($8 ~ /Z/) { print $2} }' | sort))
-
 }
 
 #run
-
 if [ -z "$zombies" ]; then 
 	#if there are no zombie processes resolve current in state
-
 	resolve
-
 else 
-
 	#if there are zombies first resolve after it create alerts
-
 	resolve
 
 	trigger
-
 fi
-
-zombies_state=`ps -aux | awk '{ if ($8 ~ /Z/) { print } }' > $path`
